9253be0df44990b78892867fde2b5623c09687b4
scenaristeur <scenaristeur@gmail.com>
2024-02-15 20:11:59 +0100

    crewai init

--- run.py
+++ run.py
@@ -2,9 +2,14 @@ import json
 from aykracy.autogen.autogen_gvt import AutogenGvt
 from aykracy.crewai.crewai_gvt import CrewaiGvt
 
+lancement = {
+    AutogenGvt: False,
+    CrewaiGvt: True
+}
+
 with open('data/gouvernement.json', encoding="utf-8") as f:
     d = json.load(f)
-    #print(d)
+    # print(d)
     # for mini in d['ministres']:
     #     print(f"mini {mini}")
 
@@ -15,10 +20,11 @@ options = {
 }
 
 
-
 def main():
-    autogenGvt = AutogenGvt(options)
-   # crewaiGvt = CrewaiGvt(options)
+    if lancement[AutogenGvt]:
+        autogenGvt = AutogenGvt(options)
+    if lancement[CrewaiGvt]:
+        crewaiGvt = CrewaiGvt(options)
 
 
 main()
